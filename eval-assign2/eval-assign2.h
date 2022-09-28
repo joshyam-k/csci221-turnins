2c4d6bde315efe80c80a43dc7f1a898d82a641bc
Joshua Yamamoto <68135138+joshyam-k@users.noreply.github.com>
2022-09-28 10:47:40 -0700

    Add files via upload

---
+++ eval-assign2/eval-assign2.h
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+int conditional(int x, int y, int z);
+int logical_negation(int a);
+int rounder(uint8_t i, int nbitsrm);
+int add_tiny(int8_t j, int8_t k);
+int multiply_tiny(int8_t a, int8_t b);
