e5ca57a67d04f214cdb0e4a5c4d94ef42b38e573
joshyam-k <yamamojo@reed.edu>
2022-09-30 13:00:00 -0700

    some work

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 #include "lab3.h"
 
+void add_to_end(linklist a, uint32_t val) {
+    node_l * current = a.head;
+    while(current->next != NULL){
+        current = current->next;
+    }
+    current->next = (node_l *)malloc(sizeof(node_l));
+    current->next->val = val;
+    current->next->next = NULL;
+
+}
+
+
+
 int main(){
+    /*a consists of a pointer called head to a node and an integer size*/
+    linklist a;
+    /*we start by setting the head pointer equal to NULL */
+    a.head = NULL;
+    /* we then allocate memory on the heap */
+    a.head = (node_l *) malloc(sizeof(node_l));
+    a.head->val = 1;
+    a.head->next = NULL;
     return 0;
 }
--- lab3/lab3.h
+++ lab3/lab3.h
@@ -4,3 +4,9 @@ typedef struct node {
     uint32_t val;
     struct node* next;
 } node_l;
+
+
+typedef struct linkedlist {
+    node_l* head;
+    int size;
+} linklist;
