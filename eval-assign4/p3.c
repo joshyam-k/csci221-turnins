a2d9005c41ef2bc6011b70a11c838af487a7dc87
joshyam-k <yamamojo@reed.edu>
2022-10-25 20:39:25 -0700

    figured out what p3 is supposed to be doing and started working on fixing code

--- eval-assign4/p3.c
+++ eval-assign4/p3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // original version of code as given to us
+/*
 int f(int x, int y){
     int r = 1;
     while(y > 1){
@@ -12,10 +13,29 @@ int f(int x, int y){
     }
     return r * x;
 }
+*/
 
 // initial thoughts:
 // the dividing by 2 and checking remainders of y makes me think this has something to do with binary
+// after running a ton of tests with different inputs on the whiteboards with drop-in tutors we realized that usually this 
+// function returns x^y, but when we tested a few edge cases (y = 0 for example) the function did not work as expected
+// thus the main work will be to adjust the code so that it works for all viable input values
 
+// one larger change will be to force y to be an unsigned integer since this algorithm simply will not work if it is negative
+int f(int x, unsigned int y){
+    int r = 1;
+    if(y == 0){
+        return 1;
+    }
+    while(y > 1){
+        if(y % 2 == 1){
+            r = x * r;
+        }
+        x = x * x;
+        y = y / 2;
+    }
+    return r * x;
+}
 
 int main(){
     int ret = f(1, 5);
