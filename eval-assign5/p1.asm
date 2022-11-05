e8f145eb83dd587334f980c479c4ffd3ee9ca2d7
joshyam-k <yamamojo@reed.edu>
2022-11-05 11:32:53 -0700

    finished file and added comments

--- eval-assign5/p1.asm
+++ eval-assign5/p1.asm
@@ -1,12 +1,14 @@
 .text
 .globl main
 main:
+    # importantly the register v0 is reserved for exit values so I store the result in t4
     li $a0, 10
     li $t0, 2  # starting index (we will compare this to n to know when to stop)
     li $t1, 0  # set f_0 = 0
     li $t2, 1     # set f_1 = 1 
-    beq $a0, $zero, Ret
+    li $t4, 0     # initialize our return value to zero 
 loop:
+    beq $a0, $zero, Exit
     slt $t3, $a0, $t0      # checking if n < i (eventually want to check i <= n)
     bne $t3, $zero, Exit   # if $t3 = 1 then i > n and so we exit
     add $t4, $t1, $t2      # f_n = f_(n-1) + f_(n-2)
