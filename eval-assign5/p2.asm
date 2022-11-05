e890f2ebd3a2675169a7b53f7c9d8d9f2e417850
joshyam-k <yamamojo@reed.edu>
2022-11-05 13:24:03 -0700

    p2 rough draft done

--- eval-assign5/p2.asm
+++ eval-assign5/p2.asm
@@ -12,7 +12,7 @@ main:
     li $t3, 0   # index of array
 loop_smaller:
     slt $t4, $t3, $a1   # $t4 = (i < length)
-    beq $t4, $zero, Exit
+    beq $t4, $zero, reset_reg1
     addi $t3, $t3, 1    # i++
     sll $t5, $t3, 2     # offset: $t5 = 4*i
     add $t6, $a0, $t5   # $t6 = a0 + 4*i
@@ -21,7 +21,7 @@ loop_smaller:
     beq $t8, $zero, loop_smaller    # if $t0 > a[i] we jump back to top of loop
     addi $s0, $s0, 1        # count++
     j loop_smaller          # return to top of loop
-reset_reg:
+reset_reg1:
     add $t3, $zero, $zero   # clear some temporary registers
     add $t4, $zero, $zero  
     add $t5, $zero, $zero
@@ -30,7 +30,7 @@ reset_reg:
     add $t8, $zero, $zero
 loop_larger:
     slt $t4, $t3, $a1   # (i < length)
-    beq $t4, $zero, Exit
+    beq $t4, $zero, reset_reg2
     addi $t3, $t3, 1    # i++
     sll $t5, $t3, 2     # t5 = 4*i
     add $t6, $a0, $t5   # a0 + 4*i
@@ -39,5 +39,58 @@ loop_larger:
     beq $t8, $zero, loop_larger    # if (a[i] > t2) is false then jump to next iteration
     addi $s1, $s1, 1    # count++
     j loop_larger
+reset_reg2:
+    add $t3, $zero, $zero
+    add $t4, $zero, $zero
+    add $t5, $zero, $zero
+    add $t6, $zero, $zero
+    add $t7, $zero, $zero
+    add $t8, $zer, $zero
+loop_range1:
+   slt $t4, $t3, $a1    # (i < length)
+   beq $t4, $zero, reset_reg3 # if i < length is false then exit the loop
+   addi $t3, $t3, 1     # i++
+   sll $t5, $t3, 2      # t5 = 4*i
+   add $t6, $a0, $t5    # t6 = a0 + 4*i
+   lw $t7, 0($t6)       # t7 = a[i]
+   slt $t8, $t7, $t0    # t8 is 0 if t0 <= a[i]
+   slti $t8, $t8, 1     # t8 is 1 if it was 0 in the previous step
+   slt $t9, $t7, $t1    # t9 is 1 if a[i] < t1
+   and $t8, $t8, $t9    # t8 is 1 if t0 < a[i] < t1
+   beq $t8, $zero, loop_range1
+   addi $s2, $s2, 1     # count ++
+   j loop_range1
+reset_reg3:
+    add $t3, $zero, $zero
+    add $t4, $zero, $zero
+    add $t5, $zero, $zero
+    add $t6, $zero, $zero
+    add $t7, $zero, $zero
+    add $t8, $zero, $zero
+    add $t9, $zero, $zero
+loop_range2:
+    slt $t4, $t3, $a1   # (i < length)
+    beq $t4, $zero, Exit   # if i < length is false then exit 
+    addi $t3, $t3, 1    # i++
+    sll $t5, $t3, 2     # t5 = 4*i
+    add $t6, $a0, $t5   # t6 = a0 + 4*i
+    lw $t7, 0($t6)      # t7 = a[i]
+    slt $t8, $t7, $t1   # t8 is 0 if t1 <= a[i]
+    slti $t8, $t8, 1    # t8 is 1 if it was 0 in the previous step
+    slt $t9, $t7, $t2   # t9 is 1 if a[i] < t2
+    and $t8, $t8, $t9   # 1 if t1 <= a[i] < t2
+    beq $t8, $zero, loop_range2
+    addi $s2, $s2, 1    # count++
+    j loop_range2
+Exit:
+    li $v0, 10
+    syscall
 
-    
+
+
+
+
+
+
+
+
