8c97aad461e0411965e9c480678eb192e0717c8e
joshyam-k <yamamojo@reed.edu>
2022-11-12 11:58:44 -0800

    weird off by 1 error

--- eval-assign6/p1.c
+++ eval-assign6/p1.c
@@ -16,7 +16,8 @@ uint32_t decay_t(uint16_t* values, uint16_t length, uint16_t decay, uint32_t tot
     if(i > length){
         return total;
     }
-    uint32_t res = values[length - i]/pow(decay, length);
+    uint32_t decayed = (uint32_t) pow(decay, length - i);
+    uint32_t res = (uint32_t) (values[length - i]/decayed);
     total = total + res;
     i++;
     return decay_t(values, length, decay, total, i);
@@ -29,27 +30,11 @@ uint32_t decay(uint16_t* values, uint16_t length, uint16_t decay){
 }
 
 
-
-
-uint32_t decay_loop(uint16_t* values, uint16_t length, uint16_t decay) {
-    uint32_t total = 0; //values[0];    // this term always included in the sum
-    for (int i = 1; i <= length; i++) {
-        uint32_t res = values[length - i]/pow(decay, length);
-        total = total + res;
-    }
-    return total;
-}
-
 int main(){
-    uint16_t values[6] = {1,2,3,4,5};
-    uint32_t test2 = decay(values, 3, 1);
-    uint32_t test = decaying_sum(values, 3, 1);
+    uint16_t values[8] = {1,2,3,20, 4,5, 10, 12};
+    uint32_t test2 = decay(values, 7, 3);
+    uint32_t test = decaying_sum(values, 7, 3);
     printf("we got %d and we expected %d", test2, test);
     return 0;
 }
 
-// length greater than 0 so continue
-// rest = decaying_sum(values + 1, 1, 10)
-// length greater than 0 so continue
-// rest = decaying_sum(values + 2, 0, 10)
-// length is 0 so return 0
