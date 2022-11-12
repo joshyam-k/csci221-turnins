9f34ea2a87a758290d451dde3770609896d6b228
joshyam-k <yamamojo@reed.edu>
2022-11-12 11:04:54 -0800

    figured out p1 tail recursion

--- eval-assign6/p1.c
+++ eval-assign6/p1.c
@@ -1,21 +1,50 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 uint32_t decaying_sum(uint16_t* values, uint16_t length, uint16_t decay){
     if(length <= 0){
         return 0;
     }
     uint32_t rest = decaying_sum(&values[1], length - 1, decay);
-    printf("currently rest is %d\n", rest);
     uint32_t decayed = rest/decay;
-    printf("returning is %d\n", values[0] + decayed);
     return values[0] + decayed;
 }
 
+// my tail recursive version
+uint32_t decay_t(uint16_t* values, uint16_t length, uint16_t decay, uint32_t total, int i){
+    if(i > length){
+        return total;
+    }
+    uint32_t res = values[length - i]/pow(decay, length);
+    total = total + res;
+    i++;
+    return decay_t(values, length, decay, total, i);
+}
+
+uint32_t decay(uint16_t* values, uint16_t length, uint16_t decay){
+    uint32_t total = 0;
+    int i = 1;
+    return decay_t(values, length, decay, total, i);
+}
+
+
+
+
+uint32_t decay_loop(uint16_t* values, uint16_t length, uint16_t decay) {
+    uint32_t total = 0; //values[0];    // this term always included in the sum
+    for (int i = 1; i <= length; i++) {
+        uint32_t res = values[length - i]/pow(decay, length);
+        total = total + res;
+    }
+    return total;
+}
+
 int main(){
-    uint16_t values[6] = {20,2,4,8,16,32};
-    uint32_t test = decaying_sum(values, 8, 10);
-    printf("res is %d", test);
+    uint16_t values[6] = {1,2,3,4,5};
+    uint32_t test2 = decay(values, 3, 1);
+    uint32_t test = decaying_sum(values, 3, 1);
+    printf("we got %d and we expected %d", test2, test);
     return 0;
 }
 
