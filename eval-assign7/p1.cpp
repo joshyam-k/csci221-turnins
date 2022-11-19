99fd7f306c83de6763b3ca470a319bf7c120851e
joshyam-k <yamamojo@reed.edu>
2022-11-19 11:29:09 -0800

    finished second function

--- eval-assign7/p1.cpp
+++ eval-assign7/p1.cpp
@@ -4,23 +4,23 @@
 #include "p1.h"
 using namespace std;
 
-double car::getx(){
+double car::getx() const{
     return x;
 }
 
-double car::gety(){
+double car::gety() const{
     return y;
 }
 
-double car::getmpg(){
+double car::getmpg() const{
     return mpg;
 }
 
-double car::gettank_size(){
+double car::gettank_size() const{
     return tank_size;
 }
 
-double car::getcurrent_fuel(){
+double car::getcurrent_fuel() const{
     return current_fuel;
 }
 
@@ -44,6 +44,30 @@ void car::setcurrent_fuel(double newcurrent_fuel){
     current_fuel = newcurrent_fuel;
 }
 
+car::car(double x_, double y_, double mpg_, double tank_size_, double current_fuel_){
+    setx(x_);
+    sety(y_);
+    setmpg(mpg_);
+    settank_size(tank_size_);
+    setcurrent_fuel(current_fuel_);
+}
+
+car::car(){
+    setx(0);
+    sety(0);
+    setmpg(0);
+    settank_size(0);
+    setcurrent_fuel(0);
+}
+
+car::car(const car &other){
+    setx(other.getx());
+    sety(other.gety());
+    setmpg(other.getmpg());
+    settank_size(other.gettank_size());
+    setcurrent_fuel(other.getcurrent_fuel());
+}
+
 bool car::move_to(double xnew, double ynew){
     double dist = sqrt(pow((xnew - getx()), 2) + pow((ynew - gety()), 2));
     double max_dist = getmpg() * getcurrent_fuel();
@@ -57,19 +81,39 @@ bool car::move_to(double xnew, double ynew){
     return false;
 }
 
-
-
+car* which_can_move(car* array, int array_length, int* return_length,  double x, double y){
+    // count how many can move
+    int n_can_move = 0;
+    for(int i = 0; i < array_length; i++){
+        car cartest = array[i];
+        if (cartest.move_to(x, y) == true){
+            n_can_move++;
+        }
+    }
+    *return_length = n_can_move;
+    // allocate space on heap for array of appropriate size and fill it with copies of the cars that can move
+    car* return_array = new car[n_can_move];
+    for(int i = 0, j = 0; i < array_length; i++){
+       car carcheck = array[i];
+       bool check = carcheck.move_to(x, y);
+       if (check == true){
+           return_array[j] = carcheck;
+           j++;
+       }
+    }
+    return return_array;
+}
 
 int main(){
-    car cartest;
-    cartest.setx(5);
-    cartest.sety(10);
-    cartest.setmpg(20);
-    cartest.setcurrent_fuel(10);
-    bool out = cartest.move_to(1500, 3000);
-    cout << cartest.getx() << "\n";
-    cout << cartest.gety() << "\n";
-    cout << cartest.getcurrent_fuel() << "\n";
+    car car1(5, 10, 20, 15, 9);
+    car car2(1, 2, 20, 15, 9);
+    car car3(0, 0, 20, 12, 0.2);
+    car car_arr[3] = {car1, car2, car3};
+    int newl = 0;
+    int* pnewl = &newl;
+    car* r_array = which_can_move(car_arr, 3, pnewl, 15, 30);
+    cout << "positon for car 1 was " << car1.getx() << "and its copy is at " << r_array[0].getx() << "\n";
+    cout << "return array is of length " << newl << "\n";
     return 0;
 }
 
--- eval-assign7/p1.h
+++ eval-assign7/p1.h
@@ -6,15 +6,18 @@ private:
     double tank_size;
     double current_fuel;
 public:
-    double getx();
-    double gety();
-    double getmpg();
-    double gettank_size();
-    double getcurrent_fuel();
+    double getx() const;
+    double gety() const;
+    double getmpg() const;
+    double gettank_size() const;
+    double getcurrent_fuel() const;
     void setx(double newx);
     void sety(double newy);
     void setmpg(double newmpg);
     void settank_size(double newtank_size);
     void setcurrent_fuel(double newcurrent_fuel);
     bool move_to(double xnew, double ynew);
+    car(double x_, double y_, double mpg_, double tank_size_, double current_fuel_);
+    car();
+    car(const car &other);
 };
